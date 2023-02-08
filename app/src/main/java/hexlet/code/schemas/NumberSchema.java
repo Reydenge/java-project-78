a0573213676a9596e5b1e4dc9b372010f9defdf6
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2023-02-08 17:15:06 +0300

    fix methods

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -3,27 +3,27 @@ package hexlet.code.schemas;
 import java.util.function.Predicate;
 
 public class NumberSchema extends BaseSchema {
-    public NumberSchema required() {
+    public final NumberSchema required() {
         this.checking = true;
         Predicate<Object> predicateChecking = x -> x instanceof Integer;
         super.addPredicate(predicateChecking);
         return this;
     }
 
-    public NumberSchema positive() {
+    public final NumberSchema positive() {
         Predicate<Object> predicateNumberSign = x -> x instanceof Integer && (int) x > 0;
         super.addPredicate(predicateNumberSign);
         return this;
     }
 
-    public NumberSchema range(int leftBorder, int rightBorder) {
+    public final NumberSchema range(int leftBorder, int rightBorder) {
         Predicate<Object> predicateRange = x -> (int) x >= leftBorder && (int) x <= rightBorder;
         super.addPredicate(predicateRange);
         return this;
     }
 
     @Override
-    boolean isInvalid(Object object) {
+    public final boolean isInvalid(Object object) {
         return !(object instanceof Integer);
     }
 }
