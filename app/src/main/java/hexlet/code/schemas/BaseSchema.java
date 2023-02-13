e15265922f1de6c95290eef9db79bf9f027657dc
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2023-02-13 18:58:35 +0300

    fix addPredicate nethod and class fields

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,15 +1,16 @@
 package hexlet.code.schemas;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema {
-    private List<Predicate<Object>> conditions = new ArrayList<>();
-    protected boolean checking;
+    private LinkedList<Predicate<Object>> conditions = new LinkedList<>();
+    protected boolean isRequired;
 
     public final boolean isValid(Object object) {
-        if (!checking && isInvalid(object)) {
+        if (!isRequired && isInvalid(object)) {
             return true;
         }
         for (Predicate<Object> predicate : conditions) {
@@ -21,7 +22,11 @@ public abstract class BaseSchema {
     }
 
     public final void addPredicate(Predicate<Object> predicate) {
-        conditions.add(predicate);
+        conditions.addFirst(predicate);
+    }
+
+    public LinkedList<Predicate<Object>> getConditions() {
+        return conditions;
     }
 
     abstract boolean isInvalid(Object object);
